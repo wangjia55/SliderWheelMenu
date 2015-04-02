b1212dc3f91e38a70e7a92231188ae9ed8de5deb
jacob-wj <wangjia199055@126.com>
2015-04-02 20:13:28 +0800

    init projects

---
+++ app/src/main/java/com/jacob/slider/wheel/MainActivity.java
@@ -0,0 +1,16 @@
+package com.jacob.slider.wheel;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+
+public class MainActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
